6de0f65cd0c78076fb9d2f9fa3c2733867bbe749
Julia Kudryavtseva <juliet.kudryavtseva@gmail.com>
2015-12-15 21:07:53 +0200

    Add filter for time

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,46 @@
  * # MainCtrl
  * Controller of the pomodoroApp
  */
-angular.module('pomodoroApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+
+var app = angular.module('pomodoroApp');
+
+app.controller('MainCtrl',  ['$scope', function($scope) {
+
+    $scope.workTime = 25;
+    $scope.breakTime = 5;
+
+    $scope.addWorkTime = function() {
+        $scope.workTime = $scope.workTime + 1;
+    }
+
+    $scope.minusWorkTime = function() {
+        if($scope.workTime > 1) {
+            $scope.workTime = $scope.workTime - 1;
+        } else {
+            $scope.workTime = 1;
+        }
+    }
+
+    $scope.addBreakTime = function() {
+        $scope.breakTime = $scope.breakTime + 1;
+    }
+
+    $scope.minusBreakTime = function() {
+        if($scope.breakTime > 1) {
+            $scope.breakTime = $scope.breakTime - 1;
+        } else {
+            $scope.breakTime = 1;
+        }
+    }
+
+    $scope.countDown = function() {
+
+    }
+
+}]);
+
+app.filter('minutesToDateTime', [function() {
+    return function(minutes) {
+        return new Date(1970, 0, 1).setMinutes(minutes);
+    };
+}])
