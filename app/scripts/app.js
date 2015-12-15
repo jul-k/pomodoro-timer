416cbf1442f0c5895470d03ae1cd6daad57ef82e
Julia Kudryavtseva <juliet.kudryavtseva@gmail.com>
2015-12-15 18:45:15 +0200

    Create basic template

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name pomodoroApp
+ * @description
+ * # pomodoroApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('pomodoroApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pomodoroApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pomodoroApp
+ */
+angular.module('pomodoroApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
